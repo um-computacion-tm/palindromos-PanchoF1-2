7486f0fed56120a3828293f93a80fa377109a561
PanchoF1 <fsadams@hotmail.com>
2024-04-05 15:35:18 -0300

    first commit

---
+++ cantidad_de_palabras_palindromes.py
@@ -0,0 +1,51 @@
+import unittest
+
+
+class TestCantidadDePalabrasPalindromes(unittest.TesCase):
+    def test_cantidad_de_palabras_palindromes_simple(self):
+        palabras = ["ala"]
+        resultado = obtener_cantidad_de_palabras_palindrome(palabras)
+        self.assertEqual(resultado, 1)
+
+    def test_cantidad_de_palabras_palindromes_con_2(self):
+        palabras = ["ala", "neuquen"]
+        resultado = obtener_cantidad_de_palabras_palindrome(palabras)
+        self.assertEqual(resultado, 2)
+
+    def test_cantidad_de_palabras_palindromes_con_3(self):
+        palabras = ["ala", "neuquen", "hola"]
+        resultado = obtener_cantidad_de_palabras_palindrome(palabras)
+        self.assertEqual(resultado, 2)
+
+    def test_cantidad_de_palabras_palindromes_con_4(self):
+        palabras = ["ala", "neuquen", "hola", "programacion"]
+        resultado = obtener_cantidad_de_palabras_palindrome(palabras)
+        self.assertEqual(resultado, 2)
+
+    def test_cantidad_de_palabras_palindromes_con_5(self):
+        palabras = ["ala", "neuquen", "hola", "programacion", "palap"]
+        resultado = obtener_cantidad_de_palabras_palindrome(palabras)
+        self.assertEqual(resultado, 3)
+
+    def test_cantidad_de_palabras_palindromes_complejo(self):
+        palabras = ["ala", "neuquen", "hola", "programacion", "palap", "neu  quen"]
+        resultado = obtener_cantidad_de_palabras_palindrome(palabras)
+        self.assertEqual(resultado, 3)
+
+    def test_cantidad_de_palabras_palindromes_complejo_2(self):
+        palabras = [
+            "ala",
+            "neuquen",
+            "hola",
+            "programacion",
+            "palap",
+            "neu  quen",
+            "agita falsos usos la fatiga",
+            "presidente de la camara de diputados: martin menem",
+			"A man, a plan, a canal - Panama"
+        ]
+        resultado = obtener_cantidad_de_palabras_palindrome(palabras)
+        self.assertEqual(resultado, 5)
+
+
+unittest.main()
